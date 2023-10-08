1d0c520cb5a853d170fce30c83031066fc4911cf
mariakuusik <maria.kuusik@gmail.com>
2023-10-08 14:52:43 +0300

    Basic table view with shipments data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Shipments table</h1>
   </div>
+
+  <table>
+    <tr>
+      <th>Orderno</th>
+      <th>Deliverydate</th>
+      <th>Customer</th>
+      <th>Trackingno</th>
+      <th>Status</th>
+      <th>Consignee</th>
+      <th></th>
+      <th></th>
+    </tr>
+    <tr v-for="shipment in shipments" :key="shipment.id">
+      <td>{{ shipment.orderNo }}</td>
+      <td>{{ shipment.date }}</td>
+      <td>{{ shipment.customer }}</td>
+      <td>{{shipment.trackingNo}}</td>
+      <td>{{shipment.status}}</td>
+      <td>{{shipment.consignee}}</td>
+      <td><button>details</button></td>
+      <td><button>delete</button></td>
+    </tr>
+  </table>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      shipments: [
+        {
+          orderNo: 0,
+          date: 0,
+          customer: '',
+          trackingNo: 0,
+          status: '',
+          consignee: ''
+        }
+      ],
+      errorResponse: ''
+    }
+  },
+  methods: {
+    async fetchShipments() {
+      try {
+        const response = await axios.get('https://my.api.mockaroo.com/shipments.json?key=5e0b62d0')
+        this.shipments = response.data
+      } catch (error) {
+        this.errorResponse = error.response.data
+      }
+    }
+  },
+
+  beforeMount() {
+    this.fetchShipments()
   }
 }
 </script>
