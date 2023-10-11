5d6a0ce8b34d4a0c4e6507bbc8082892702c8f68
mariakuusik <maria.kuusik@gmail.com>
2023-10-11 15:06:58 +0300

    Some more custom CSS added, unnecessary fields removed

--- src/components/ShipmentDetailsModal.vue
+++ src/components/ShipmentDetailsModal.vue
@@ -8,34 +8,33 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-
           <div class="container">
             <div class="row">
               <div class="col-md-6">
                 <div class="mb-3 text-start">
-                  <label for="input1">Order No</label>
+                  <label for="input1" class="form-label">Order No</label>
                   <input v-model="selectedShipment.orderNo" type="text" class="form-control" id="input1">
                 </div>
                 <div class="mb-3 text-start">
-                  <label for="input2">Customer</label>
+                  <label for="input2" class="form-label">Customer</label>
                   <input v-model="selectedShipment.customer" type="text" class="form-control" id="input2">
                 </div>
                 <div class="mb-3 text-start">
-                  <label for="input3">Consignee</label>
+                  <label for="input3" class="form-label">Consignee</label>
                   <input v-model="selectedShipment.consignee" type="text" class="form-control" id="input3">
                 </div>
               </div>
               <div class="col-md-6 text-start">
                 <div class="mb-3">
-                  <label for="input4">Date</label>
+                  <label for="input4" class="form-label">Date</label>
                   <input v-model="selectedShipment.date" type="text" class="form-control" id="input4">
                 </div>
                 <div class="mb-3 text-start">
-                  <label for="input5">Tracking No</label>
+                  <label for="input5" class="form-label">Tracking No</label>
                   <input v-model="selectedShipment.trackingNo" type="text" class="form-control" id="input5">
                 </div>
                 <div class="mb-3 text-start">
-                  <label for="dropdown">Status</label>
+                  <label for="dropdown" class="form-label">Status</label>
                   <select v-model="selectedShipment.status" class="form-select" id="dropdown">
                     <option value="Shipped">Shipped</option>
                     <option value="In Transit">In Transit</option>
